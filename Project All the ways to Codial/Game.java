37e9b55f4da6489662a8609cede1d4095410752d
TheScientist1300 <109092276+TheScientist1300@users.noreply.github.com>
2023-02-03 16:12:26 +0200

    update

--- Project All the ways to Codial/Game.java
+++ Project All the ways to Codial/Game.java
@@ -10,6 +10,16 @@ return this.choise;
 public void setchoise(int choise) {
   this.choise = choise;
 }
+// public void printCharByChar(String promt) {
+//   for (int i = 0; i < promt.length(); i++) {
+//       System.out.print(promt.charAt(i));
+//       try {
+//           Thread.sleep(100);
+//       } catch (InterruptedException e) {
+//           e.printStackTrace();
+//       }
+//   }
+// }
 public void run() {
 Level l1 = new Level("Hello fellow travaller, welcome to Codial! Here we live in peace and harmony with each other, ints and Strings, trues and falses, static with voids and more! We are known for our various trading routs going through our land. This is why so many people say 'All the ways lead to Codial', ah...You don't know this sentence...Well now you know! So the reason we called for your help was to get something we lost in one of our main trading route, it seems emptey today so I don't think it will be a problem to go and check. Oh! I almost forgot, so that we will be in touch take this wierd communication device, the people here decided to call it '01110111 01100001 01101100 01101011 01111001 0001110100 01100001 01101100 01101011 01100101 01111001'...They are Codials they always name stuff in binaric language, for example house they call '01101000 01101111 01110101 01110011 01100101'. So anyway, I will guide you twoards your adventure, and good luck!...(an hour later)...So, I see you are coming close to the middle of the road, it's really close to you! (about an hour later) So maybe I was wrong and the road changes his length each time 'THE PROGRAMMER!' changes the length of it, Wait, DO YOU NOT KNOW WHO THE PROGRAMMER IS?!!! Okay relax sysout, yes this is my name, I was acually named after my father syserr from the Cody Cidle, it's pretty much our belith in Codial, in which we belive that our world was made by the programmer in a month, making all of the classes in our programm. Okay, back to the main thing, it seems you are between two sideways, the one on the left is the way to 'Virsudial', a place where all of the viruses live in, and the way to the right is to the 'Defendial', which they are our protectors. Which way will you like to go through? (1 to Defendial and 2 to Virsudial", null, null);
 Level l2l = new Level("Wait...You tell me you will prefer to go through a place full of viruses instead of going...Okay you know what? I don't care, I don't care! As long and you will find the 'gem of dot' I don't care, oh I didn't tell you about the 'gem of dot'? Well the gem of dot is a gem that can grant an infinte life span. Cool right? It was sent here by a waggon but when it got to here, it wasn't there! And don't think of using it! It is beyond your power! Okay so I will contact you when I will dedtect a signal of the gem....(an hour later)...Wait! I read a signal, I am not sure it's that gem but it's worth to find out... Oh they are two sideways again well I can't see them on my map, take a wild guess. (1 for left and 2 for right)", null, null);
--- Project All the ways to Codial/Level.java
+++ Project All the ways to Codial/Level.java
@@ -18,7 +18,7 @@ public class Level {
 
  }
  public void setPrompt(String prompt) {
-    this.prompt = prompt;  
+    this.prompt = prompt; 
  }
 
  public Level getLeft() {
